d8d742ecf042a0b7233e55a5a75ef2cfddeae6cf
Lindarleon <159889211+Lindarleon@users.noreply.github.com>
2025-07-23 01:58:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
+
+const app = express();
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { amount } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Stripe server running");
+});
